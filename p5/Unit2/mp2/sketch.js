372c45a9d4347da284a595b75d864848a46b3a90
joogunn <joogunn@ilstu.edu>
2020-10-08 11:16:45 -0500

    new

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -2,7 +2,7 @@ var img;
 var x = 0;
 var mic;
 var vol;
-//let myState = 0;
+let myState = 0;
 
 function setup() {
   createCanvas(800, 800);
@@ -16,47 +16,77 @@ function setup() {
 
 function draw() {
   background('black');
-//  switch (myState) {
-  //  case 0:
-    //  fill('white');
-      //text("DO NOT YELL", 250, 250);
-      //break;
 
-  //  case 1:
-    //  fill('white');
-    //  text("I'M MOVING", 250, 250);
-    //  break;
+  vol = (mic.getLevel()).toFixed(2);
 
-  //  case 2:
-    //  fill('white');
-    //  text("YOU'RE TOO LOUD", 250, 250);
-    //  break;
-      vol = (mic.getLevel()).toFixed(2);
+  switch (myState) {
+    case 0:
+    textSize(50);
+      fill('white');
+      text("Hello, this is Creepy Cat! Click to get started", 250, 250);
+      cat();
+      break;
+
+
+    case 1:
+    textSize(50);
+      fill('white');
+
+
+      text("I'M MOVING", 250, 250);
+
 
       if (vol > .001) { // if the volume is LOUD?
         // do something
 
-        x = x + 10;
+        x = x + (5 * vol * 100);
       }
+
+      if (vol > .2) {
+        myState = 2;
+      }
+
 
       push();
       translate(x, 0);
 
       cat();
-      x = x + 5;
+
       if (x > width) {
         x = -300;
       }
       pop();
 
+      break;
 
+    case 2:
+    textSize(50);
+      fill('white');
+      text("ALRIGHT I'M GONE", 250, 250);
+      break;
   }
 
-  function cat() {
-    translate(0, 100);
-    image(img, width / 2, height / 2, 200, 200);
+
+
+
+}
+
+
+function cat() {
+  translate(0, 100);
+  image(img, width / 2, height / 2, 200, 200);
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
+
+
+function mouseReleased() {
+
+  myState++;
+  if (myState > 2) {
+    myState = 0;
   }
 
-  function touchStarted() {
-    getAudioContext().resume();
-  }
+}
