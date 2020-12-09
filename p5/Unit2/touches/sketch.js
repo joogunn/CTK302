03ce260bf4db9880a910526eefaa696589b8b24e
joogunn <joogunn@ilstu.edu>
2020-12-08 23:18:51 -0600

    touches

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -1,12 +1,18 @@
 let numberOfTouches ;
+var img1, img2;
+let state = 0;
 
 function setup() {
   createCanvas(400, 400);
+
+  img1 = loadImage("assets/fingers-two.jpg");
+  img2 = loadImage("assets/threefingers.jpg");
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
-  numberOfTouches = touches.length; //automatically available to us 
+  numberOfTouches = touches.length; //automatically available to us
   text(numberOfTouches + ' touches', 5, 10);
 
   switch(numberOfTouches) {
@@ -17,19 +23,28 @@ function draw() {
       case 1:
        text("it's kind of lonely here", 5, 22) ;
       // put a picture here
+
       break ;
 
       case 2:
       text("two fingers are touching the screen", 5, 22) ;
             // put a picture here
+            image(img1, width/2, height/2, 200, 200);
       break ;
 
       case 3:
      text("three are touching the screen", 5, 22) ;
+     image(img2, width/2, height/2, 200,200);
             // put a picture here
       break ;
 
 
   }
 
+}
+function mouseReleased(){
+  state++;
+  if(state>3){
+    state = 0 ;
+  }
 }
